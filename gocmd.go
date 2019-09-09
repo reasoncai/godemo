6fa49a08f2e5290fc06d8cc802c1422899fe3897
reasoncai <403411876@qq.com>
2019-09-09 23:43:29 +0800

    init

---
+++ gocmd.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+func main() {
+	cmd := exec.Command("/Users/reason/PycharmProjects/common-tool/bin/ffmpeg", "-version")
+	buf, err := cmd.Output()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println(string(buf))
+}
